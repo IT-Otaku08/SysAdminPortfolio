29c0702b0fd9a019262ba2da1849b7d75896d18b
Daniel Conaway <dconaway976@gmail.com>
2026-09-24 18:01:05 -0500

    Refactor user creation script to use parameters

--- Basic User Creation.ps1
+++ Basic User Creation.ps1
@@ -7,6 +7,9 @@ $Password = Read-Host -Prompt "Default Password:" -AsSecureString
 # Specify user account storage location
 $OUpath = "OU=Users,OU=Home,DC=home,DC=local"
 
+#Retrieve Domain Information
+$Domain = (Get-ADDomain).DNSRoot
+
 $ExitRequested = ""
 
 while ($ExitRequested -ne "Q")
@@ -17,16 +20,20 @@ while ($ExitRequested -ne "Q")
     $UserName = $FirstName[0] + $LastName
 
     # Create new AD account for each user
-    New-ADUser `
-        -Name "$FirstName $LastName" `
-        -GivenName $FirstName `
-        -Surname $LastName `
-        -SamAccountName "$UserName" `
-        -UserPrincipalName "$UserName@home.local" `
-        -Path $OUpath `
-        -AccountPassword $Password `
-        -ChangePasswordAtLogon $true `
-        -Enabled $true
+    $Params = 
+    @{
+        Name = "$FirstName $LastName"
+        GivenName = $FirstName
+        Surname = $LastName
+        SamAccountName = $UserName
+        UserPrincipalName = "$UserName@$Domain"
+        Path = $OUpath
+        AccountPassword = $Password
+        ChangePasswordAtLogon = $true
+        Enabled = $true
+    }
+
+    New-ADUser @Params
 
     # Loop exit?
     $ExitRequested = Read-Host -Prompt "Press 'Q' to end account creation: "
